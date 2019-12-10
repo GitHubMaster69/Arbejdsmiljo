fc933a91bd506e92fffd660789911c71237854f4
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2019-12-10 12:31:31 +0100

    Basic structure

---
+++ Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Main.java
@@ -0,0 +1,16 @@
+package Arbejdsmiljoe;
+import processing.core.PApplet;
+public class Main extends PApplet {
+    protected final PApplet p = this;
+    public static void main(String[] args){ Main.main("Arbejdsmiljoe.Main"); }
+    public void settings(){
+        size(800,800);
+    }
+    @Override
+public void draw(){
+    p.background(100);
+}
+public void mouseClicked(){
+
+}
+}
