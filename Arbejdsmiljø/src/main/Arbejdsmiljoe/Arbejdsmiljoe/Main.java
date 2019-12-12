5a64a39b2447e2b474144d086f88a4959cf3a551
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2019-12-12 14:14:59 +0100

    SWING GUI

--- Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Main.java
+++ Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Main.java
@@ -2,16 +2,23 @@ package Arbejdsmiljoe;
 import processing.core.PApplet;
 public class Main extends PApplet {
     protected final PApplet p = this;
-    public static void main(String[] args){ Main.main("Arbejdsmiljoe.Main"); }
-public static MainDisplay Displays = new MainDisplay();
+    public static void main(String[] args){
+        Main.main("Arbejdsmiljoe.Main");
+
+
+    }
+    public static MainDisplay Displays = new MainDisplay();
+   // public static MyGUIForm loginscreen = new MyGUIForm(); ikke slet, er igang <3
 
     public void settings(){
-        fullScreen();
+        size(displayWidth/4,displayHeight/4);
     }
 
     public void draw()  {
     p.background(100);
     Displays.Display();
+
+
 
 
     }
