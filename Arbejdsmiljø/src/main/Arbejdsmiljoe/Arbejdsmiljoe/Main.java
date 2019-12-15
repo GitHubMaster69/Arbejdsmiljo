c4e6ce1e4d31af2d44a3fc58410039595116557d
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2019-12-15 22:02:36 +0100

    #3 Remote database test
    Hvis vi kan bruge en ekstern database så betyder det at vi kan sende elevernes svar til læreren.

--- Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Main.java
+++ Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Main.java
@@ -10,24 +10,28 @@ public class Main extends PApplet {
     public void settings(){
         size(800,800);
         SQLiteTest db = new SQLiteTest();
-        try {
-            db.getQuestion("sikkerhed", "1");
+        /*try {
+            //db.getQuestion("sikkerhed", "1");
+            //db.initializeRemoteDatabase(1);
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-        }
+        }*/
     }
     //@Override
-public void draw(){
-    p.background(100);
-}
+    public void draw(){
+        p.background(100);
+    }
 public void mouseClicked(){
     SQLiteTest db = new SQLiteTest();
+    db.initializeRemoteDatabase(1);
+    /*
     try {
         ResultSet questionSQL = db.getQuestion("sikkerhed", "1");
         System.out.println(questionSQL.getString(1));
-        db.storeQuestion(2, "sikkerhed", "why are you gay?", "i am not gay", "you are gay", "what the fuck", "no you are fuck");
+        db.storeQuestion(2, "sikkerhed", "why are you gay lmao?", "i am not gay", "you are gay", "what the fuck", "no you are fuck");
     } catch (SQLException | ClassNotFoundException e) {
         e.printStackTrace();
     }
+    */
 }
 }
