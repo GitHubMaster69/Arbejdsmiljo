b542cb60ea6ac8c3d270c3cd7fbf86162f7befc1
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2019-12-15 21:56:10 +0100

    Diplay class structure

--- Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Display.java
+++ Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/Display.java
@@ -1,4 +1,21 @@
 package Arbejdsmiljoe;
 
-public class Display {
+import processing.core.PApplet;
+
+public class Display extends MainDisplay {
+    protected final PApplet p = this;
+
+public void loginScreen() {
+
+}
+public void quizList() {
+
+}
+public void takeQuiz() {
+
+}
+public void seeQuiz() {
+
+}
+
 }
--- Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/MainDisplay.java
+++ Arbejdsmiljø/src/main/Arbejdsmiljoe/Arbejdsmiljoe/MainDisplay.java
@@ -4,16 +4,38 @@ import processing.core.PApplet;
 
 public class MainDisplay extends PApplet {
     protected final PApplet p = this;
+    public static Display loginScreen = new Display();
+    public static Display takeQuiz = new Display();
+    public static Display quizList = new Display();
+    public static Display seeQuiz = new Display();
 
+
+    public int page;
 
     public void Display(){
-
-
-
     }
 
-
-
-
-
+    public void displaySelector() {
+     switch (page) {
+         case 0:
+             loginScreen.loginScreen();
+             break;
+         case 1:
+             quizList.quizList();
+             break;
+         case 2:
+             takeQuiz.takeQuiz();
+             break;
+         case 3:
+             seeQuiz.seeQuiz();
+             break;
+         }
+    }
 }
+
+
+
+
+
+
+
